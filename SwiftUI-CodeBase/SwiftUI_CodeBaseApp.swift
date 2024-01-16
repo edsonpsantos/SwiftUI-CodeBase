56f10d2b22feb7ab27d2a0b7d1b8113fdb3626cb
epdsant <epdsant@gmail.com>
2024-01-16 14:44:06 +0000

    Initial commit

---
+++ SwiftUI-CodeBase/SwiftUI_CodeBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CodeBaseApp.swift
+//  SwiftUI-CodeBase
+//
+//  Created by EDSON SANTOS on 16/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CodeBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
